faaa5024ce8bae9962a60bae3a0add139182ff0a
IhorDobryanskiy <l48507100120@gmail.com>
2023-02-05 21:53:05 +0200

    some fix

--- src/js/app.js
+++ src/js/app.js
@@ -137,167 +137,25 @@ function createCellsGrid(width, height) {
     const index = row * width + col;
     return minesIndex.includes(index);
   }
-  // cellArray = [];
-  // flagsCounter = 0;
-
-  // minesCounter = 0;
-
-  // let cellCount = 0;
-
-  // let isFirstClick = true;
-
-  // let openCellCount = 0;
-
-  // mineField.innerHTML = "";
-
-  // if (columnsInput.value <= 7 || rowsInput.value <= 7) {
-  //   alert("Размеры поля слишком маленькие.");
-  //   return;
-  // } else if (columnsInput.value > 16 || rowsInput.value > 16) {
-  //   alert("Размеры поля слишком большие.");
-  //   return;
-  // }
-
-  // for (let x = 0; x < rows; x++) {
-  //   cellArray[x] = [];
-  //   for (let y = 0; y < columns; y++) {
-  //     const cell = createElement("div", "cell");
-  //     cell.setAttribute("data-mine", "0");
-  //     cell.setAttribute("data-flag", "0");
-  //     mineField.append(cell);
-  //     cellCount++;
-  //     cellArray[x].push(cell);
-  //   }
-  // }
-
-  // for (
-  //   let i = 0;
-  //   i < ;
-  //   i++
-  // ) {
-  //   cellArray[getRandomInt(cellArray.length)][
-  //     getRandomInt(cellArray[0].length)
-  //   ].dataset.mine = "1";
-  // }
-
-  // cellArray.forEach((element) => {
-  //   element.forEach((cell) => {
-  //     if (+cell.getAttribute("data-mine") === 1) {
-  //       minesCounter++;
-  //     }
-  //   });
-  // });
-
-  // function openCell(row, column) {
-  //   if (!isValidPosition(...arguments)) {
-  //     return;
-  //   }
-
-  //   if (isBomb(...arguments)) {
-  //     return;
-  //   }
-
-  //   const minesCount = bombsAroundCell(...arguments);
-
-  //   if (minesCount === 0) {
-  //     cellArray[row][column].style.opacity = 0;
-  //     openCell(row, column - 1);
-  //     openCell(row - 1, column - 1);
-  //     openCell(row - 1, column);
-  //     openCell(row - 1, column + 1);
-  //     openCell(row, column + 1);
-  //     // openCell(row + 1, column - 1);
-  //     // openCell(row + 1, column);
-  //     // openCell(row + 1, column + 1);
-  //     return;
-  //   }
-
-  //   cellArray[row][column].innerHTML = minesCount;
-  // }
-
-  // function bombsAroundCell(row, column) {
-  //   let bombsCount = 0;
-  //   for (let x = -1; x <= 1; x++) {
-  //     for (let y = -1; y <= 1; y++) {
-  //       isValidPosition(row + y, column + x) &&
-  //         isBomb(row + y, column + x) &&
-  //         bombsCount++;
-  //     }
-  //   }
-  //   return bombsCount;
-  // }
-
-  // function isBomb(row, column) {
-  //   return (
-  //     (+cellArray[row][column].getAttribute("data-mine") === 1 && true) || false
-  //   );
-  // }
-
-  // function isValidPosition(row, column) {
-  //   return (
-  //     (cellArray.includes(cellArray[row]) &&
-  //       cellArray[row].includes(cellArray[row][column]) &&
-  //       true) ||
-  //     false
-  //   );
-  // }
-
-  // showMines(cellArray);
-
-  // cellArray.forEach((row, rowIndex) => {
-  //   row.forEach((cell, columnIndex) => {
-  //     cell.addEventListener("click", () => {
-  //       openCell(rowIndex, columnIndex);
-  //       // } else if (openCellCount === cellCount - minesCounter) {
-  //       //   const winModalWindow = createElement("div", "win");
-  //       //   const winVideo = createElement("video", "win-video");
-  //       //   const startAgain = createElement("button", "again-button");
-  //       //   winVideo.src = "@files/videoplayback.mp4";
-  //       //   winVideo.setAttribute("autoplay", "true");
-  //       //   winVideo.setAttribute("loop", "true");
-  //       //   startAgain.innerText = "Закрыть и начать заного";
-  //       //   winModalWindow.append(winVideo, startAgain);
-  //       //   startAgain.addEventListener("click", () => {
-  //       //     document.location.reload();
-  //       //   });
-  //       //   app.append(winModalWindow);
-  //       // }
-  //     });
-
-  //     cell.addEventListener("contextmenu", (e) => {
-  //       e.preventDefault();
-  //       if (
-  //         cell.style.backgroundColor === "red" ||
-  //         cell.style.backgroundColor === "pink"
-  //       )
-  //         return;
-  //       if (cell.dataset.flag === "0") {
-  //         cell.dataset.flag = "1";
-  //         cell.innerHTML = "";
-  //         cell.innerHTML = FLAG;
-  //         flagsCounter++;
-  //         flags.innerText = `Количество установленных флагов: ${flagsCounter}`;
-  //       } else if (cell.dataset.flag === "1") {
-  //         cell.dataset.flag = "0";
-  //         cell.innerHTML = "";
-  //         flagsCounter--;
-  //         flags.innerText = `Количество установленных флагов: ${flagsCounter}`;
-  //       }
-  //     });
-  //   });
-  // });
-
-  // mines.innerText = `Количество мин: ${minesCounter}`;
 }
 
 buttonReset.addEventListener("click", () => {
   flagsCounter = 0;
   minesCounter = 0;
+  buttonStart.style.display = "block";
 });
 
 buttonStart.addEventListener("click", (e) => {
   e.preventDefault();
-  flagsCounter = 0;
-  minesCounter = 0;
-  createCellsGrid(columnsInput.value, rowsInput.value);
+  if (
+    rowsInput.value >= 8 &&
+    rowsInput.value <= 16 &&
+    columnsInput.value >= 8 &&
+    columnsInput.value <= 16
+  ) {
+    flagsCounter = 0;
+    minesCounter = 0;
+    createCellsGrid(columnsInput.value, rowsInput.value);
+    buttonStart.style.display = "none";
+  } else alert("Введите правильное число строк и столбцов");
 });
